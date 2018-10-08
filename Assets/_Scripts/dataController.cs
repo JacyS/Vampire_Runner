be470767b0d542b665d86b742944d0be155e981c
Daniel Osgood <d.osgood@hotmail.co.uk>
2018-10-08 21:00:36 +0100

    DataControllerWIP
    Requires data to be parsed from other scripts but should in theory write and read, couple little things need to be fixed but as of this point is just awaiting save data

--- Assets/_Scripts/dataController.cs
+++ Assets/_Scripts/dataController.cs
@@ -1,16 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
 
-public class dataController : MonoBehaviour {
+[System.Serializable]
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class AllDataStore {
+
+}
+
+public class DataController : MonoBehaviour
+
+
+{
+    public string path;
+    public string jsonString;
+
+
+    public AllDataStore allData = new AllDataStore();
+}
+
+void Start()
+{
+    DontDestroyOnLoad(transform.gameObject);
+    path = Application.streamingAssetsPath + "/SaveData.json";
+    //SaveAllData();
+    //LoadAllData();
+}
+
+
+public void SaveAllData() {
+
+    File.WriteAllText(path, JsonUtility.ToJson(allData));
+}
+
+
+public void LoadAllData() {
+    jsonString = File.ReadAllText(path);
+    allData = JsonUtility.FromJson<AllDataStore>(jsonString);
 }
