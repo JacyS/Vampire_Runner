0ce8b7fe0d2e2eb7d751917212fa15f053302016
Unknown <d.osgood@hotmail.co.uk>
2018-11-11 16:15:41 +0000

    Anti-Infinite Jump code implemented into player controller
    can't jump forever now

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -10,18 +10,29 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody2D myRigidbody;
 
 
+    public bool grounded;
+    public LayerMask whatIsGround;
+
+    private Collider2D myCollider;
+
 	// Use this for initialization
 	void Start () {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
         myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
 
         if (Input.GetKeyDown(KeyCode.Space)) //add mobile input here for mobile testing later. (||) use those to define OR later, your stupid keyboard doesnt have them. 
         {
-            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+            if (grounded)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+            }
         }
 	}
 }
