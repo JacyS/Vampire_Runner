68d8a0038a25b112d22823a08f9bc9e267c393bf
Unknown <d.osgood@hotmail.co.uk>
2018-11-11 15:55:07 +0000

    Player Movement, Spawning Not Done Yet, also adding in unity standard assets so we can do mobile controls
    Has basic movement, moving, jumping, and standard assets for mobile controls

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D myRigidbody;
+
+
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space)) //add mobile input here for mobile testing later. (||) use those to define OR later, your stupid keyboard doesnt have them. 
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+        }
+	}
+}
