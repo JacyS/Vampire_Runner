42ee098e10de20c017fe495c87af9027ff7b6de2
LapisLazuli <07pearcehan2@gmail.com>
2018-11-12 14:03:08 +0000

    Added some more functionality to the menus.

--- Assets/_Scripts/MainMenuController.cs
+++ Assets/_Scripts/MainMenuController.cs
@@ -3,52 +3,72 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MainMenuController : MonoBehaviour {
+public class MainMenuController : MonoBehaviour
+{
 
-    public Button StartButton, OptionsButton, HSButton, OptionsBack, HighscoreBack;
-    public GameObject MMenuContain;
-    public GameObject OptionsContain;
-    public GameObject HighscoreContain;
+    public Button StartButton, OptionsButton, HSButton, OptionsBack, HighscoreBack, StartBack, PlayButton;
+    public GameObject MMenuContain, OptionsContain, HighscoreContain, SkinsContain, PowerUpContain, GameContain;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-        Button Start = StartButton.GetComponent<Button>();
-        Button Options = OptionsButton.GetComponent<Button>();
-        Button Highscore = HSButton.GetComponent<Button>();
-
-        MMenuContain.SetActive(true);
+        MMenuContain.SetActive(true); //Shows Main Menu as default
         OptionsContain.SetActive(false);
         HighscoreContain.SetActive(false);
-        
+        SkinsContain.SetActive(false);
+        PowerUpContain.SetActive(false);
+        GameContain.SetActive(false);
 
-	}
+    }
 
     public void StartClick()
     {
         MMenuContain.SetActive(false);
         OptionsContain.SetActive(false);
         HighscoreContain.SetActive(false);
+        SkinsContain.SetActive(true); //Shows Skins Menu
+        PowerUpContain.SetActive(false);
+        GameContain.SetActive(false);
     }
 
     public void OptionsClick()
     {
         MMenuContain.SetActive(false);
-        OptionsContain.SetActive(true);
+        OptionsContain.SetActive(true); //Shows Options Menu
         HighscoreContain.SetActive(false);
+        SkinsContain.SetActive(false);
+        PowerUpContain.SetActive(false);
+        GameContain.SetActive(false);
     }
 
     public void HSClick()
     {
         MMenuContain.SetActive(false);
         OptionsContain.SetActive(false);
-        HighscoreContain.SetActive(true);
+        HighscoreContain.SetActive(true); //Shows Highscore Menu
+        SkinsContain.SetActive(false);
+        PowerUpContain.SetActive(false);
+        GameContain.SetActive(false);
     }
 
     public void BackClick()
     {
-        MMenuContain.SetActive(true);
+        MMenuContain.SetActive(true); //Shows Main Menu
         OptionsContain.SetActive(false);
         HighscoreContain.SetActive(false);
+        SkinsContain.SetActive(false);
+        PowerUpContain.SetActive(false);
+        GameContain.SetActive(false);
     }
+
+    public void PowersClick()
+    {
+        MMenuContain.SetActive(false);
+        OptionsContain.SetActive(false);
+        HighscoreContain.SetActive(false);
+        SkinsContain.SetActive(false);
+        PowerUpContain.SetActive(true); //Shows Power-up Menu
+        GameContain.SetActive(false);
+    }
+
 }
