45e26306895cd5eff4e86ca475a45198a1f4fc92
JacyS <jacys@live.co.uk>
2018-11-27 14:50:26 +0000

    Player Mechanics
    I have made it so the player can now slide for x seconds (and have a reload on when they can next slide for y seconds). Jump and slide animations that player correctly. 
    Death animation when the palyer collides with a hazard. (also the player stops moving)
    Added Gravestone, Spikes and Crossbow Bolt hazards to the game (they are tagged as 'hazard').
    Added a DevTestingObj which allows you to restart by pressing R.
    Changed the jump and sliding inputs to X and Z and changed a few things around in the player controller script to make it better hopefully.
    Player animation controller and animations.

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -8,33 +8,100 @@ public class PlayerController : MonoBehaviour {
     public float jumpForce;
 
     private Rigidbody2D myRigidbody;
-
+    private Collider2D myCollider;
+    private Animator myAnimator;
 
     public bool grounded;
+    bool sliding;
     public LayerMask whatIsGround;
 
-    private Collider2D myCollider;
+    bool JumpButton;
+    bool SlideButton;
 
-	// Use this for initialization
-	void Start () {
+    public float slideTime;
+    float currentSlideTime;
+    bool canSlide;
+    public float slidingReload;
+    float currentSlidingReload;
+
+    bool playerDead;
+
+    // Use this for initialization
+    void Start () {
         myRigidbody = GetComponent<Rigidbody2D>();
         myCollider = GetComponent<Collider2D>();
+        myAnimator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        GetInputButtons();
         grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
 
         myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space)) //add mobile input here for mobile testing later. (||) use those to define OR later, your stupid keyboard doesnt have them. 
+        if (JumpButton && grounded && !sliding) { 
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+        }
+
+        if (SlideButton && grounded && !sliding && canSlide)
         {
-            if (grounded)
+            sliding = true;
+            currentSlideTime = 0;
+        }
+
+        if (sliding)
+        {
+            currentSlideTime += Time.deltaTime;
+            if (currentSlideTime >= slideTime)
             {
-                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+                sliding = false;
+                canSlide = false;
+                currentSlidingReload = slidingReload;
             }
         }
-	}
+
+        if (!canSlide)
+        {
+            currentSlidingReload -= Time.deltaTime;
+            if (currentSlidingReload <= 0)
+            {
+                canSlide = true;
+            }
+        }
+
+        //set animation
+        myAnimator.SetBool("isGrounded", grounded);
+        myAnimator.SetBool("isSliding", sliding);
+        myAnimator.SetBool("isDead", playerDead);
+    }
+
+    void GetInputButtons()
+    {
+        JumpButton = false;
+        SlideButton = false;
+
+        //Jump down
+        if (Input.GetKeyDown(KeyCode.Z)){ //add mobile input here for mobile testing later. (||) use those to define OR later, your stupid keyboard doesnt have them. 
+            JumpButton = true;
+        }
+
+        //Sliding button down
+        if (Input.GetKey(KeyCode.X))
+        { //add mobile input here for mobile testing later. (||) use those to define OR later, your stupid keyboard doesnt have them. 
+            SlideButton = true;
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "hazard")
+        {
+            moveSpeed = 0;
+            playerDead = true;
+        }
+    }
+
 
     public void GetPowerup()
     {
