b8c126e71dccfe5560ff338a988e557850caf797
Unknown <d.osgood@hotmail.co.uk>
2018-12-02 17:24:44 +0000

    Pickup generator implemented
    Now spawns basic pickups on a 15% chance, can be made lower or higher easily

---
+++ Assets/_Scripts/PickupGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupGenerator : MonoBehaviour
+{
+    public GameObject Platform1;
+    public GameObject Pickup1;
+    public float distanceBetween;
+    public float RandomNum;
+    public Transform generationPoint;
+    float timeToGo;
+    private float platformWidth;
+    
+
+    void Start()
+    {
+        platformWidth = Platform1.GetComponent<BoxCollider2D>().size.x;
+        timeToGo = Time.fixedTime + 0.75f;
+    }
+
+    void FixedUpdate()
+    {
+        RandomNum = Random.Range(0, 19);
+        if (Time.fixedTime >= timeToGo)
+        {
+            if (RandomNum <= 3 && (transform.position.x < generationPoint.position.x))
+            {
+                transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+
+                Instantiate(Pickup1, transform.position, transform.rotation);
+            }
+
+            timeToGo = Time.fixedTime + 0.75f;
+        }
+
+    }
+}
--- Assets/_Scripts/PlatformGenerator.cs
+++ Assets/_Scripts/PlatformGenerator.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlatformGenerator : MonoBehaviour {
 
     public GameObject Platform1;
+    public GameObject Platform2;
+    public GameObject Platform3;
     public Transform generationPoint;
     public float distanceBetween;
     public float RandomNum;
@@ -16,13 +18,13 @@ public class PlatformGenerator : MonoBehaviour {
     // Use this for initialization
     void Start() {
         platformWidth = Platform1.GetComponent<BoxCollider2D>().size.x;
-        timeToGo = Time.fixedTime + 1.0f;
+        timeToGo = Time.fixedTime + 0.75f;
 
     }
 
     // Update is called once per frame
     void FixedUpdate() {
-        RandomNum = Random.Range(1, 20);
+        RandomNum = Random.Range(0, 19);
         if (Time.fixedTime >= timeToGo)
         {
             if (RandomNum <= 10 && (transform.position.x < generationPoint.position.x))
@@ -31,9 +33,22 @@ public class PlatformGenerator : MonoBehaviour {
 
                 Instantiate(Platform1, transform.position, transform.rotation);
             }
-            timeToGo = Time.fixedTime + 1.0f;
 
+            else if (RandomNum <= 15 && (transform.position.x < generationPoint.position.x))
+            {
+                transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
 
+                Instantiate(Platform2, transform.position, transform.rotation);
+            }
+            
+            else if (RandomNum <20 && (transform.position.x < generationPoint.position.x))
+            {
+                transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+
+                Instantiate(Platform3, transform.position, transform.rotation);
+            }
+
+            timeToGo = Time.fixedTime + 0.75f;
         }
 
        
