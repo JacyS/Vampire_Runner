83e114355e1613ba3cf4c0bf186f5d97c74e10b5
JacyS <jacys@live.co.uk>
2019-01-03 20:32:32 +0000

    crossbow and sounds fixed

---
+++ Assets/Sound/CLICK_MENU.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+
+public class CLICK_MENU : MonoBehaviour {
+
+    public AudioClip click_Sound;
+
+    //GameObject myaudiosource;
+
+    private Button button { get { return GetComponent<Button>(); } }
+    private AudioSource source;
+
+    // Use this for initialization
+    void Start () {
+        source = GameObject.Find("SFXobj").GetComponent<AudioSource>();
+	}
+
+    void PlaySound()
+    {
+        source.PlayOneShot(click_Sound);
+    }
+}
